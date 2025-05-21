6d115ccbaa6cc327590b337a75c25ce862c9f27a
DanielM2303 <dmoreno685@fafi.utb.edu.ec>
2025-05-20 23:51:21 -0500

    agg DEBUG=True comentado

--- proyectoDjango/settings.py
+++ proyectoDjango/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-uc%@3eryznu!j7b#9kv@ns^d&gxs)@!azckzg**f7gl8=1m@-1')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = False
+# DEBUG = True
 # ALLOWED_HOSTS = ['*']
 
 # DESPLIEGUE RENDER
